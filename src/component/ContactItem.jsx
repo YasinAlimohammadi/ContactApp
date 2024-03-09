d5423b35f96252d4197d042d4d7cc8c4190ad5bd
Yasin AliMohammadi <tscosport@gmail.com>
2024-03-09 22:06:54 +0330

    Add component ContactItem.jsx

---
+++ src/component/ContactItem.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { HiOutlineMail } from "react-icons/hi";
+import { FiPhone } from "react-icons/fi";
+import { FaRegTrashCan } from "react-icons/fa6";
+
+const ContactItem = ({data: {id, name, lastName, email, phone}}) => {
+    return (
+        <div>
+            <li key={id}>
+                    <p>{name} {lastName}</p>
+                    <p><span><HiOutlineMail/></span>{email}</p>
+                    <p><span><FiPhone/></span>{phone}</p>
+                    <p><button><FaRegTrashCan/></button></p>
+            </li>
+        </div>
+    );
+};
+
+export default ContactItem;
